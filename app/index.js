ce79fcaeb18576d267af01efb27e161d13eb1906
bobvantpadje <bobvantadje@gmail.com>
2017-02-01 13:01:08 +0100

    reload in browser test for server

--- app/index.js
+++ app/index.js
@@ -17,7 +17,7 @@ ReactDOM.render(
 
     <Provider store={store}>
         {/* hashHistory ---> browserHistory when the site is on a server for clean urls*/}
-        <Router history={hashHistory}>
+        <Router history={history}>
             <Route path="/" component={App}>
                 <IndexRoute component={Home} />
                 <Route path="/Login" component={UserIsNotAuthenticated(Login)} />
